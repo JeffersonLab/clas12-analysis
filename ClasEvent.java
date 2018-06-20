3056450b237eda7f4150255000ccb2b760c2f657
Noraim <noraim@jlab.org>
2018-06-20 15:47:29 -0400

    Clas Event
    completed

---
+++ ClasEvent.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class ClasEvent {
+	long eventNumber;
+	int runNumber;
+	ArrayList<ClasParticle> particles;
+	double beamEnergy;
+	int targetType;
+        float sttime; 
+        float rftime;
+        float bcg;
+        long trig; 
+        
+        
+	public long getEventNumber() {
+		return eventNumber;
+	}
+	public void setEventNumber(long eventNumber) {
+		this.eventNumber = eventNumber;
+	}
+	public int getRunNumber() {
+		return runNumber;
+	}
+	public void setRunNumber(int runNumber) {
+		this.runNumber = runNumber;
+	}
+	public ArrayList<ClasParticle> getParticles() {
+		return particles;
+	}
+	public void setParticles(ArrayList<ClasParticle> particles) {
+		this.particles = particles;
+	}
+	public double getBeamEnergy() {
+		return beamEnergy;
+	}
+	public void setBeamEnergy(double beamEnergy) {
+		this.beamEnergy = beamEnergy;
+	}
+	public int getTargetType() {
+		return targetType;
+	}
+	public void setTargetType(int targetType) {
+		this.targetType = targetType;
+	}
+	
+}
