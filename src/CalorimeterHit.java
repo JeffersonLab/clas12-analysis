d3c5be19812a240b79ee6754d445719afcd90089
William Phelps <willphelps@gmail.com>
2018-06-21 18:07:09 -0400

    Created DetectorHit object
    Consolidates common data between detectors.

--- src/CalorimeterHit.java
+++ src/CalorimeterHit.java
@@ -4,20 +4,7 @@
  * Noraim Nunez (CSUDH), June 20, 2018 
  */
 
-public class CalorimeterHit {
-
-    int index;  //#16 Index of the hit in the specific detector bank
-    int pindex; //#16 Row number in the particle bank hit is associated with
-    int detector;//#8 Detector ID,defined in COATJAVA DetectorType 
-    int sector; //#8 Sector of the Detector hit
-    int layer;  //#8 Layer of the Detector hit 
-    float energy; //Energy associated with the hit (GeV)
-    float time; //Time associated with the hit (ns)
-    float path; //Path from vertex to the hit position (cm)
-    float chi2; //Quality of hit-track matching
-    float x; //X coordinate of the hit (cm)
-    float y; //Y coordinate of the hit (cm)
-    float z; //Z coordinate of the hit (cm)
+public class CalorimeterHit extends DetectorHit{
     float hx; //X coordinate of the matched hit (cm)
     float hy; //Y coordinate of the matched hit (cm)
     float hz; //Z coordinate of the matched hit (cm)
@@ -35,78 +22,6 @@ public class CalorimeterHit {
     float m3w; //3rd moment of the shower on W-side
     int status; //Hit status 
 
-    public int getindex() {
-        return index;
-    }
-    public void setindex(int index) {
-        this.index = index;
-    }
-    public int getpindex() {
-        return pindex;
-    }
-    public void setpindex(int pindex) {
-        this.pindex = pindex;
-    }
-    public int getdetector() {
-        return detector;
-    }
-    public void setdetector(int detector) {
-        this.detector = detector;
-    }    
-    public int getsector() {
-        return sector;
-    }
-    public void setsector(int sector) {
-        this.sector = sector;
-    }
-    public float getlayer() {
-        return layer;
-    } 
-    public void setlayer(int layer) {
-        this.layer = layer;
-    }
-    public float getenergy() {
-        return energy;
-    }
-    public void setenergy(float energy) {
-        this.energy = energy;
-    } 
-    public float gettime() {
-        return time; 
-    }
-    public void settime(float time) {
-        this.time = time;
-    } 
-    public float getpath() {
-        return chi2; 
-    }
-    public void setpath(float path) {
-        this.path = path;
-    } 
-    public float getchi2() {
-        return chi2; 
-    }
-    public void setchi2(float chi2) {
-        this.chi2 = chi2;
-    } 
-    public float getx() {
-        return x; 
-    }
-    public void setx(float x) {
-        this.x = x;
-    } 
-    public float gety() {
-        return y; 
-    }
-    public void sety(float y) {
-        this.y = y;
-    }
-    public float getz() {
-        return z; 
-    }
-    public void setz(float z) {
-        this.z = z;
-    }
     public float gethx() {
         return hx; 
     }
---
+++ src/DetectorHit.java
@@ -0,0 +1,88 @@
+
+public class DetectorHit {
+	private int index; // #16 Index of the hit in the specific detector bank
+	private int pindex; // #16 Row number in the particle bank hit is associated with
+	private int detector;// #8 Detector ID,defined in COATJAVA DetectorType
+	private int sector; // #8 Sector of the Detector hit
+	private int layer; // #8 Layer of the Detector hit
+	private float energy; // Energy associated with the hit (GeV)
+	private float time; // Time associated with the hit (ns)
+	private float path; // Path from vertex to the hit position (cm)
+	private float chi2; // Quality of hit-track matching
+	private float x; // X coordinate of the hit (cm)
+	private float y; // Y coordinate of the hit (cm)
+	private float z; // Z coordinate of the hit (cm)
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public int getPindex() {
+		return pindex;
+	}
+	public void setPindex(int pindex) {
+		this.pindex = pindex;
+	}
+	public int getDetector() {
+		return detector;
+	}
+	public void setDetector(int detector) {
+		this.detector = detector;
+	}
+	public int getSector() {
+		return sector;
+	}
+	public void setSector(int sector) {
+		this.sector = sector;
+	}
+	public int getLayer() {
+		return layer;
+	}
+	public void setLayer(int layer) {
+		this.layer = layer;
+	}
+	public float getEnergy() {
+		return energy;
+	}
+	public void setEnergy(float energy) {
+		this.energy = energy;
+	}
+	public float getTime() {
+		return time;
+	}
+	public void setTime(float time) {
+		this.time = time;
+	}
+	public float getPath() {
+		return path;
+	}
+	public void setPath(float path) {
+		this.path = path;
+	}
+	public float getChi2() {
+		return chi2;
+	}
+	public void setChi2(float chi2) {
+		this.chi2 = chi2;
+	}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public float getZ() {
+		return z;
+	}
+	public void setZ(float z) {
+		this.z = z;
+	}
+}
