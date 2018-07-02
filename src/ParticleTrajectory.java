40216af69c783b3137899ecf4aecffa97b33c688
Noraim <noraim@jlab.org>
2018-07-02 15:06:39 -0400

    Particle Trajectory

--- src/ParticleTrajectory.java
+++ src/ParticleTrajectory.java
@@ -5,110 +5,49 @@
  */
 public class ParticleTrajectory { //extends ParticleTrack
     
-    // same name as detector components 
-    private int index; // position of the track at the detector surface (cm)
-    private int pindex; // 
-    private float x; // position of the track at the detector surface (cm)
-    private float y; // 
-    private float z; // 
+    // relates to detector 
+    private int index; // #16 Index of the hit in the specific detector bank
+    private int pindex; // #16 Row number in the particle bank hit is associated with
+    private float x; // X coordinate of the hit (cm)
+    private float y; // Y coordinate of the hit (cm)
+    private float z; // Z coordinate of the hit (cm)
 //
-    
     //relates to particletrack 
     private int q; //charge of the track
-
-    
     private int detId; 
     private float cx;//direction cosline of the track at the detector surface
     private float cy;
     private float cz;
     private float pathlength;//pathlength of the track to the detector surface from the DOCA point (cm)
 
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public int getPindex() {
-        return pindex;
-    }
-
-    public void setPindex(int pindex) {
-        this.pindex = pindex;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public float getZ() {
-        return z;
-    }
-
-    public void setZ(float z) {
-        this.z = z;
-    }
-
-    public int getQ() {
-        return q;
-    }
-
-    public void setQ(int q) {
-        this.q = q;
-    }
-
-    public int getDetId() {
+    public int getdetId() {
         return detId;
+    } 
+    public void setdetId(int detId) {
+	this.detId = detId;
     }
-
-    public void setDetId(int detId) {
-        this.detId = detId;
+    public float getcx() {
+	return cx;
     }
-
-    public float getCx() {
-        return cx;
+    public void setcx(float cx) {
+	this.cx = cx;
     }
-
-    public void setCx(float cx) {
-        this.cx = cx;
+    public float getcy() {
+	return cy;
     }
-
-    public float getCy() {
-        return cy;
+    public void setcy(float cy) {
+		this.cy = cy;
     }
-
-    public void setCy(float cy) {
-        this.cy = cy;
+    public float getcz() {
+	return cz;
     }
-
-    public float getCz() {
-        return cz;
+    public void setcz(float cz) {
+	this.cz = cz;
     }
-
-    public void setCz(float cz) {
-        this.cz = cz;
+    public void setpathlength(float pathlength) {
+		this.pathlength = pathlength;
     }
-
-    public float getPathlength() {
-        return pathlength;
-    }
-
-    public void setPathlength(float pathlength) {
-        this.pathlength = pathlength;
-    }
-} 
-   
+    public float getpathlength() {
+	return pathlength;
+	}   
+}
